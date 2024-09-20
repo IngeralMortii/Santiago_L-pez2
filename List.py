27d515d1effcf4a360dea2c0439e183dd8b680b5
IngeralMorti <ingeral826@gmail.com>
2024-09-20 11:12:51 -0600

    upload

---
+++ List.py
@@ -0,0 +1,28 @@
+#Homogeneus list
+integers = [1, 2, 3, 8, 33]
+
+animals = ["dog", "cat", "goat"]
+
+names = ["Juan", "Felix", "Carlos"]
+
+float = [2.2, 4.5, 9.8, 10.8]
+
+#Heterogeneus List
+
+numbers_animals_names = [2, "cat", 34.33, "Travis"]
+
+
+list_lists = [[1,2,3], ["cat", "dog", "goat"]]
+
+
+#How to access an element in a list 
+
+list = [3, 22, 30, 5.3, 20]
+
+print(list[2])
+
+print(list[0])
+
+print(list[1])
+
+print(list[4])
