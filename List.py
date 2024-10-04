3e6556a416227aeb775badc7037e108ff2ad03ae
IngeralMorti <ingeral826@gmail.com>
2024-10-04 10:43:06 -0600

    upload

--- List.py
+++ List.py
@@ -26,3 +26,32 @@ print(list[0])
 print(list[1])
 
 print(list[4])
+
+
+list_of_squares =[]
+for int in range (1,10):
+    square = int **2
+    list_of_squares.append(square)
+    
+print(list_of_squares)
+
+#[expression for item in list if condition]
+
+#squared2=[item**2 for item in range (1,10)]
+
+#print(squared2)
+
+#numbers = [1,2,3,4,5,6,7,8,9]
+
+
+#cubic = [number**3 for number in numbers]
+#print(cubic)
+
+
+
+
+numbers = [1,2,3,4,5,6,7,8,9]
+
+doubled_numbers = [num*2 for num in numbers if num%2 == 0]
+
+print(doubled_numbers)
