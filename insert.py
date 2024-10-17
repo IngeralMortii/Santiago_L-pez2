b6706cc791fffe553a99a753b300e7808743fcff
IngeralMorti <ingeral826@gmail.com>
2024-10-17 11:16:51 -0600

    upload

---
+++ insert.py
@@ -0,0 +1,21 @@
+a = ['Master', 'online']
+a.insert(1,"code")
+print(a)
+
+
+c =[4,5,6]
+d =[1,2,3]
+c.insert(0,d)
+print(c)
+
+
+company = ["Santiago", "Azahel", "Juan", "Carlos", "Raul", "Gabriel"]
+company.sort(reverse=True, key=len)
+#company.sort(reverse=True)
+#company.sort()
+print(company)
+
+
+
+
+
